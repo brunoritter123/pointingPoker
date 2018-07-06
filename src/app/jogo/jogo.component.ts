4080b3b601404169a081734c4da9d396929df140
Bruno Ritter <brunosk8123@hotmail.com>
2018-07-05 21:39:37 -0300

    ajustes no layout

--- src/app/jogo/jogo.component.ts
+++ src/app/jogo/jogo.component.ts
@@ -6,17 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jogo.component.css']
 })
 export class JogoComponent implements OnInit {
-  public pontos: Array<any> = [
-    {class: 'thf-md-12' , label: '0'},
-    {class: 'thf-md-12' , label: '1'},
-    {class: 'thf-md-12' , label: '2'},
-    {class: 'thf-md-12' , label: '3'}
+  public cartas: Array<any> = [
+    {value: 1 , label: '1 ponto'},
+    {value: 2 , label: '2 pontos'},
+    {value: 3 , label: '3 pontos'},
+    {value: 5 , label: '5 pontos'},
+    {value: 8 , label: '8 pontos'},
+    {value: 13 , label: '13 pontos'},
+    {value: 21 , label: '21 pontos'},
+    {value: 54 , label: '54 pontos'},
+    {value: undefined   , label: '?'},
   ];
+
+  public pontuacao: Array<any> = [
+    {ponto: '1' , votos: 3},
+    {ponto: '2' , votos: 2},
+    {ponto: '?' , votos: 1}
+  ];
+
+  public media = 13;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.pontos);
   }
 
 }
