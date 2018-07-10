88fa34923ec5aa6bcf8d9f4903e8258d07920048
Bruno Ritter <brunosk8123@hotamil.com>
2018-07-10 13:37:42 -0300

    ajustes nas cartas e lista de jogadores

--- src/app/jogo/jogo.component.ts
+++ src/app/jogo/jogo.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoComponent implements OnInit {
   public cartas: Array<any> = [
-    {value: 1 , label: '1 ponto'},
-    {value: 2 , label: '2 pontos'},
-    {value: 3 , label: '3 pontos'},
-    {value: 5 , label: '5 pontos'},
-    {value: 8 , label: '8 pontos'},
-    {value: 13 , label: '13 pontos'},
-    {value: 21 , label: '21 pontos'},
-    {value: 54 , label: '54 pontos'},
+    {value: 1 , label: '1'  , type: 'default'},
+    {value: 2 , label: '2'  , type: 'default'},
+    {value: 3 , label: '3'  , type: 'default'},
+    {value: 5 , label: '5'  , type: 'default'},
+    {value: 8 , label: '8'  , type: 'default'},
+    {value: 13 , label: '13', type: 'default'},
+    {value: 21 , label: '21', type: 'default'},
+    {value: 54 , label: '54', type: 'default'},
     {value: undefined   , label: '?'},
   ];
 
@@ -24,6 +24,13 @@ export class JogoComponent implements OnInit {
     {ponto: '?' , votos: 1}
   ];
 
+  public jogadores: Array<any> = [
+    {nome: 'Bruno'   , voto: 3},
+    {nome: 'Luciano' , voto: 2},
+    {nome: 'Cris'    , voto: 1},
+    {nome: 'Jorte'   , voto: 1}
+  ];
+
   public media = 13;
 
   constructor() { }
@@ -31,4 +38,13 @@ export class JogoComponent implements OnInit {
   ngOnInit() {
   }
 
+  public cartaClick(value: number): void {
+    for (let i = 0; i < this.cartas.length; i++) {
+      if (this.cartas[i].value === value) {
+        this.cartas[i].type = 'danger';
+      } else {
+        this.cartas[i].type = 'default';
+      }
+    }
+  }
 }
