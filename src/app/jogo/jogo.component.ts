1928556a260e8f39be2d739ea89e13bcaba03d2a
Bruno Ritter <brunosk8123@hotamil.com>
2018-07-31 13:30:56 -0300

    ajustes

--- src/app/jogo/jogo.component.ts
+++ src/app/jogo/jogo.component.ts
@@ -3,6 +3,7 @@ import { JogoService } from './jogo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Carta } from '../models/carta.model';
 import { User } from '../models/user.model';
+import { Estatistica } from '../models/estatistica.model';
 
 @Component({
   selector: 'app-jogo',
@@ -20,12 +21,8 @@ export class JogoComponent implements OnInit, OnDestroy {
   public fimJogo: boolean;
   public cartas: Array<Carta> = [];
   public jogadores: Array<User> = [];
-  public media = 13;
-  public pontuacao: Array<any> = [
-    {ponto: '1' , votos: 3},
-    {ponto: '2' , votos: 2},
-    {ponto: '?' , votos: 1}
-  ];
+  public maisVotado: string = undefined;
+  public pontuacao: Array<Estatistica> = undefined;
 
   private conUsers;
   private conCartas;
@@ -33,6 +30,7 @@ export class JogoComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.fimJogo = false;
+    this.pontuacao = undefined;
     const nomeUser = this.route.snapshot.params['nomeUser'];
 
     this.conUsers = this.jogoService.getUsersConnect(nomeUser).subscribe( (users: Array<User>) => {
@@ -49,6 +47,11 @@ export class JogoComponent implements OnInit, OnDestroy {
         for (let i = 0; i < this.cartas.length; i++) {
           this.cartas[i].type = 'default';
         }
+        this.pontuacao = undefined;
+        this.maisVotado = undefined;
+
+      } else {
+        this.geraEstatistica();
       }
     });
 
@@ -80,4 +83,40 @@ export class JogoComponent implements OnInit, OnDestroy {
   public resetClick(): void {
     this.jogoService.sendReset();
   }
+
+  private geraEstatistica(): void {
+    let existeArray: boolean;
+    let novoPonto: Estatistica;
+
+    this.pontuacao = [];
+
+    this.jogadores.forEach(jogador => {
+      existeArray = false;
+
+      this.pontuacao.forEach(ponto => {
+        if (ponto.label === jogador.voto.label ) {
+          existeArray = true;
+          ponto.votos += 1;
+        }
+      });
+
+      if (!existeArray) {
+        novoPonto = new Estatistica(jogador.voto.label, 1);
+        this.pontuacao.push(novoPonto);
+      }
+
+      this.pontuacao.sort( (a: Estatistica , b: Estatistica) => {
+        let ret: number = b.votos - a.votos;
+        if (ret === 0) {
+          ret = b. - a.votos
+        }
+
+        return (b.votos - a.votos);
+      });
+
+      this.maisVotado = this.pontuacao[0].label;
+    });
+
+  }
+
 }
