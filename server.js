161f4c52a1627f30b64c833f79ead299413192c5
brunoritter123 <brunosk8123@hotmail.com>
2018-08-01 06:39:46 -0300

    ajuste da porta

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-let io = require('socket.io').listen(server);
+
 
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'))
 app.get('/*', (req,res) => res.sendFile(path.join(__dirname)));
 const server = http.createServer(app);
+
 server.listen(port, () => console.log('Running...'));
+let io = require('socket.io').listen(server);
 
 let users  = []
 const votoNull = {value: null, label: null, type: null};
